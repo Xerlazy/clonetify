4990d95f4cb8b4b99659bf1944840385c85323ac
xerlazy <steeve.bernard73@gmail.com>
2023-11-22 21:15:50 +0100

    reussi a faire le menu nurger

--- js/scriptNav.js
+++ js/scriptNav.js
@@ -1,8 +1,26 @@
 /* Activation du menu hambuger */
 
-let menu = document.querySelector('.myLinks')
+let menuLeft = document.querySelector('.myLinks')
 let btn = document.querySelector('.icoBurger')
 
+let menuCatalog = document.querySelector('#catalog')
+let btnC = document.querySelector('.close')
+
+
 btn.addEventListener('click', function () {
-    menu.classList.toggle('activeLink')
+    // quand on clic sur icoBurger il se passe deux chose:
+        // on ajoute la classe hidden a catalogue
+    menuCatalog.classList.toggle('hidden')
+        // on ajoute la classe qui modifie la taille du menu left
+    menuLeft.classList.toggle('activeLink')
+})
+
+
+
+btnC.addEventListener('click', function () {
+    // quand on clic sur l'icone close il se passe deux chose:
+        // on retire la classe hidden a catalogue
+    menuCatalog.classList.toggle('hidden')
+        // on retir la classe qui modifie la taille du menu left
+    menuLeft.classList.toggle('activeLink')
 })
